4a41dd2f2cc4c65fda6d016405b66170163f07a7
Sarkin <kinan.sarmini@gmail.com>
2015-08-07 04:08:02 +0400

    Utilities

--- src/utilities.cpp
+++ src/utilities.cpp
@@ -1,13 +1,32 @@
 #include "utilities.h"
 
+#include <iomanip>
+#include <iterator>
 #include <sstream>
 
-void HexTo64(const std::string &source, std::string &destination) {
+std::ostream& operator << (std::ostream& os, const ByteContainer& bc) {
+    std::copy(bc.begin(), bc.end(), std::ostream_iterator<int>(os, " "));
+    return os;
 }
 
-void BytesToHex(const ByteContainer &source, std::string &destination) {
-
+void AsciiToBytes(const std::string& source, ByteContainer& destination) {
+    destination.assign(source.begin(), source.end());
 }
 
-void HexToBytes(const std::string &hex, ByteContainer &dest) {
+void HexTo64(const std::string& source, std::string& destination) {
+}
+
+void BytesToHex(const ByteContainer& source, std::string& destination) {
+    destination.clear();
+
+    std::stringstream hexStringStream;
+    hexStringStream << std::hex << std::setfill('0');
+    for (size_t i = 0; i < source.size(); ++i) {
+        hexStringStream << std::setw(2) << static_cast<int>(source[i]);
+    }
+
+    destination = hexStringStream.str();
+}
+
+void HexToBytes(const std::string& hex, ByteContainer& dest) {
 }
--- src/utilities.h
+++ src/utilities.h
@@ -3,21 +3,26 @@
 #ifndef CRYPTOGRAPHY_CRYPTO_UTILITIES_H_
 #define CRYPTOGRAPHY_CRYPTO_UTILITIES_H_
 
+#include <iostream>
 #include <string>
-
-#include "types.h"
+#include <vector>
 
 typedef unsigned char byte;
 typedef std::vector<byte> ByteContainer;
 
-// Converts an ASCII string to Base64 string.
-void HexTo64(const std::string &source, std::string &destination);
+// Prints ByteContainer elements space-separated
+std::ostream& operator << (std::ostream& os, const ByteContainer& bc);
 
-// Converts a sequence of bytes to an ASCII string.
-void BytesToHex(const ByteContainer &source, std::string &destination);
+// Converts an ASCII string to a sequence of bytes
+void AsciiToBytes(const std::string& source, ByteContainer& destination);
+
+// Converts an ASCII string to Base64 string.
+void HexTo64(const std::string& source, std::string& destination);
+
+// Converts a sequence of bytes to a hex string.
+void BytesToHex(const ByteContainer& source, std::string& destination);
 
 // Converts a hex string to a sequence of bytes.
-void HexToBytes(const std::string &hex, ByteContainer &dest);
-
+void HexToBytes(const std::string& hex, ByteContainer& destination);
 
 #endif // CRYPTOGRAPHY_CRYPTO_UTILITIES_H_
