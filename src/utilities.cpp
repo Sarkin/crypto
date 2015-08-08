87455e6c7d3bb21c4e3b9444a778148827af85e7
Sarkin <kinan.sarmini@gmail.com>
2015-08-08 22:42:30 +0400

    Utilities

--- src/utilities.cpp
+++ src/utilities.cpp
@@ -13,20 +13,60 @@ void AsciiToBytes(const std::string& source, ByteContainer& destination) {
     destination.assign(source.begin(), source.end());
 }
 
-void HexTo64(const std::string& source, std::string& destination) {
+int Base64Value(int c) {
+    if (c < 0 || c >= 64) {
+        return c;
+    } else if (c < 26) {
+        return c + 'A';
+    } else if (c < 52) {
+        return c - 26 + 'a';
+    } else if (c < 62) {
+        return c - 52 + '0';
+    } else if (c == 62) {
+        return '+';
+    }
+    return '/';
+}
+
+void BytesTo64(const ByteContainer& source, std::string& destination) {
+    destination.clear();
+
+    size_t size = source.size();
+    for (size_t i = 0; i < size; ) {
+        int octets[3] = {source[i++], ((i < size)? source[i++]: 0), ((i < size)? source[i++]: 0)};
+        int octet_concatenation = (octets[0] << 16) | (octets[1] << 8) | octets[2];
+
+        destination += Base64Value(octet_concatenation >> 18);
+        destination += Base64Value((octet_concatenation >> 12) & 0x3f);
+        destination += Base64Value((octet_concatenation >> 6) & 0x3f);
+        destination += Base64Value(octet_concatenation & 0x3f);
+    }
 }
 
 void BytesToHex(const ByteContainer& source, std::string& destination) {
     destination.clear();
 
-    std::stringstream hexStringStream;
-    hexStringStream << std::hex << std::setfill('0');
+    std::stringstream hex_string_stream;
+    hex_string_stream << std::hex << std::setfill('0');
     for (size_t i = 0; i < source.size(); ++i) {
-        hexStringStream << std::setw(2) << static_cast<int>(source[i]);
+        hex_string_stream << std::setw(2) << static_cast<int>(source[i]);
     }
 
-    destination = hexStringStream.str();
+    destination = hex_string_stream.str();
 }
 
-void HexToBytes(const std::string& hex, ByteContainer& dest) {
+void HexToBytes(const std::string& hex, ByteContainer& destination) {
+    destination.clear();
+
+    std::stringstream hex_string_stream;
+    for (size_t i = 0; i < hex.length(); ) {
+        hex_string_stream.clear();
+        const char tmpValue[3] = {hex[i++], hex[i++], '\0'};
+        hex_string_stream.str(tmpValue);
+        hex_string_stream >> std::hex;
+
+        int next_byte;
+        hex_string_stream >> next_byte;
+        destination.push_back(next_byte);
+    }
 }
--- src/utilities.h
+++ src/utilities.h
@@ -7,22 +7,25 @@
 #include <string>
 #include <vector>
 
-typedef unsigned char byte;
-typedef std::vector<byte> ByteContainer;
+typedef unsigned char Byte;
+typedef std::vector<Byte> ByteContainer;
+
+// Returns Base64 represenation of c. Returns c is c is invalid.
+int Base64Value(int c);
 
 // Prints ByteContainer elements space-separated
 std::ostream& operator << (std::ostream& os, const ByteContainer& bc);
 
-// Converts an ASCII string to a sequence of bytes
+// Converts an ASCII string to a sequence of Bytes
 void AsciiToBytes(const std::string& source, ByteContainer& destination);
 
-// Converts an ASCII string to Base64 string.
-void HexTo64(const std::string& source, std::string& destination);
+// Converts a sequence of bytes to a Base64 string.
+void BytesTo64(const ByteContainer& source, std::string& destination);
 
-// Converts a sequence of bytes to a hex string.
+// Converts a sequence of Bytes to a hex string.
 void BytesToHex(const ByteContainer& source, std::string& destination);
 
-// Converts a hex string to a sequence of bytes.
+// Converts a hex string to a sequence of Bytes.
 void HexToBytes(const std::string& hex, ByteContainer& destination);
 
 #endif // CRYPTOGRAPHY_CRYPTO_UTILITIES_H_
