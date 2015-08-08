a154ee8ae0616cae69845b53103670f91b1d6fe9
Sarkin <kinan.sarmini@gmail.com>
2015-08-09 01:17:03 +0400

    Added some utilities unit tests

--- test/utilities_unittest.cpp
+++ test/utilities_unittest.cpp
@@ -1,9 +1,61 @@
 #include <algorithm>
+#include <string>
+#include <vector>
 
 #include "../src/utilities.h"
 #include "gtest/gtest.h"
 
 TEST(Base64Value, OutOfRange) {
     EXPECT_EQ(-1, Base64Value(-1));
-    EXPECT_EQ(0, Base64Value(5));
+    EXPECT_EQ(64, Base64Value(64));
+    EXPECT_EQ(-100, Base64Value(-100));
+    EXPECT_EQ(100, Base64Value(100));
+}
+
+TEST(Base64Value, Correctness) {
+    EXPECT_EQ('A', Base64Value(0));
+    EXPECT_EQ('Z', Base64Value(25));
+    EXPECT_EQ('a', Base64Value(26));
+    EXPECT_EQ('z', Base64Value(51));
+    EXPECT_EQ('0', Base64Value(52));
+    EXPECT_EQ('9', Base64Value(61));
+    EXPECT_EQ('+', Base64Value(62));
+    EXPECT_EQ('/', Base64Value(63));
+}
+
+// BytesToHex(HexToBytes(a)) == a
+TEST(BytesToHex, Consistency) {
+    std::string hex = "a0ffb9c7001122338591f7b1c4";
+    ByteContainer bc;
+    HexToBytes(hex, bc);
+    std::string tmp;
+    BytesToHex(bc, tmp);
+    EXPECT_EQ(hex, tmp);
+}
+
+// HexToBytes(BytesToHex(a)) == a
+TEST(HexToBytes, Consistency) {
+    ByteContainer bc;
+    for (size_t i = 0; i < (1 << 8); ++i) {
+        bc.push_back(i);
+    }
+    std::string hex;
+    BytesToHex(bc, hex);
+    ByteContainer tmp;
+    HexToBytes(hex, tmp);
+    EXPECT_EQ(bc, tmp);
+}
+
+TEST(BytesToHex, SimpleByteSequence) {
+    ByteContainer bc = {0, 1, 2, 16, 161};
+    std::string hex;
+    BytesToHex(bc, hex);
+    EXPECT_EQ("00010210a1", hex);
+}
+
+TEST(HexToBytes, SimpleHexString) {
+    std::string hex = "00010210a1";
+    ByteContainer bc;
+    HexToBytes(hex, bc);
+    EXPECT_EQ(ByteContainer({0, 1, 2, 16, 161}), bc);
 }
